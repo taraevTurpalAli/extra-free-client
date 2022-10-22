5bf817eb1e3efb4b133e07b70ab0806cad3800dd
Khamzaatov <adamkhamzatov9522@gmail.com>
2022-10-22 16:07:38 +0300

    Создал компоненты и страницу

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import header from './header.module.sass'
+
+const Header = () => {
+    return (
+        <div className={header.container}>
+            
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,11 @@
+import section from './section.module.sass'
+
+const Section = () => {
+    return (
+        <div className={section.container}>
+            
+        </div>
+    );
+};
+
+export default Section;
